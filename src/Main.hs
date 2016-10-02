fa44c3b442f02f0ee0f2f9bbd8bdd42a2474e55f
Michael Gattozzi <mgattozzi@gmail.com>
2016-10-02 15:06:54 -0400

    Add string example

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,8 @@
 import FLib
+import Foreign.C.String (newCString)
 
 main :: IO ()
 main = do
   putStrLn $ show $ doubleInput 3
+  str <- newCString "Hello World\0"
+  printString str
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
+use std::ffi::CString;
+
 #[no_mangle]
-pub extern "C" fn double_input(x: i32) -> i32 {
+pub extern fn double_input(x: i32) -> i32 {
     2 * x
+}
+
+#[no_mangle]
+pub extern fn print_string(x: CString) {
+    if let Ok(input) = x.into_string() {
+        println!("We're printing strings from Rust!");
+        println!("{}",input);
+    } else {
+        panic!("Unable to print input");
+    }
 }
